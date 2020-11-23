4754814d840d23ac6b7855ec3c2ae0febe88b418
Karl Gutwin <karl@bioteam.net>
2020-11-23 09:38:34 -0500

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+pushd acpanel
+yarn && yarn build
+popd
+rm -rf lambdas/static
+cp -r acpanel/build lambdas/static
+find lambdas -type f | xargs chmod 644
+find lambdas -type d | xargs chmod 755
+
+sam build
+
+sam deploy --s3-bucket sam-deployments-941817831 \
+    --stack-name acpanel \
+    --capabilities CAPABILITY_IAM
